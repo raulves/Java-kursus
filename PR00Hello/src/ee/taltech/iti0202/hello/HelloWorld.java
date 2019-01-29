52dd63472984d21dbf9b6cbfa3cdd670c7a55c6d
Raul Vesinurm <raul.vesinurm@ttu.ee>
2019-01-29 20:15:53 +0200

    HelloWorld

---
+++ PR00Hello/src/ee/taltech/iti0202/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package ee.taltech.iti0202.hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello world!" to console window.
+        System.out.println("Hello, world!");
+    }
+}
