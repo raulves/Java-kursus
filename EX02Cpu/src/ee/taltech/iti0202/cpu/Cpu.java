0858dadab302e5437b5f9744784ac0fd783b8ca5
Raul Vesinurm <raul.vesinurm@ttu.ee>
2019-02-07 12:43:18 +0200

    EX02CPU

--- EX02Cpu/src/ee/taltech/iti0202/cpu/Cpu.java
+++ EX02Cpu/src/ee/taltech/iti0202/cpu/Cpu.java
@@ -29,25 +29,22 @@ public class Cpu {
 
     public static void main(String[] args) {
 
-        System.out.println(registers("b inc 5 if a > 1\n" +
-                "a inc 1 if b < 5\n" +
-                "c dec -10 if a >= 1\n" +
-                "c inc -20 if c == 10"));
-
-        var res = compute(
+        Map<String, Integer> res = compute(
                 "b inc 5 if a > 1\n" +
-                "a inc 1 if b < 5\n" +
-                "c dec -10 if a >= 1\n" +
-                "c inc -20 if c == 10");
+                        "a inc 1 if b < 5\n" +
+                        "c dec -10 if a >= 1\n" +
+                        "c inc -20 if c == 10"
+        );
         System.out.println(res); // {a=1, b=0, c=-10}
 
         res = compute(
                 "b inc 7 if a > 4\n" +
-                "a inc 1 if c < 13\n" +
-                "c dec -10 if a >= 1\n" +
-                "c inc -20 if c == 10\n" +
-                "abc inc 100 if a != -23\n" +
-                "a inc 2 if a <= 0");
+                        "a inc 1 if c < 13\n" +
+                        "c dec -10 if a >= 1\n" +
+                        "c inc -20 if c == 10\n" +
+                        "abc inc 100 if a != -23\n" +
+                        "a inc 2 if a <= 0"
+        );
         System.out.println(res); // {a=1, b=0, c=-10, abc=100}
 
 
