7aaabbef0fd7c90e98147882ae56fd3a8f7bf4d3
Raul Vesinurm <raul.vesinurm@ttu.ee>
2019-02-16 13:34:19 +0200

    EX03

--- EX03SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetworkDemo.java
+++ EX03SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetworkDemo.java
@@ -1,7 +1,7 @@
 package ee.taltech.iti0202.socialnetwork;
-import ee.taltech.iti0202.socialnetwork.group.Group;
-import ee.taltech.iti0202.socialnetwork.message.Message;
-import ee.taltech.iti0202.socialnetwork.user.User;
+// import ee.taltech.iti0202.socialnetwork.group.Group;
+// import ee.taltech.iti0202.socialnetwork.message.Message;
+// import ee.taltech.iti0202.socialnetwork.user.User;
 
 public class SocialNetworkDemo {
 
