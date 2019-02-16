90f735086e4871a88d8f210fe582fa1a26b5f24f
Raul Vesinurm <raul.vesinurm@ttu.ee>
2019-02-16 13:33:23 +0200

    EX03

--- EX03SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetworkDemo.java
+++ EX03SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetworkDemo.java
@@ -6,6 +6,7 @@ import ee.taltech.iti0202.socialnetwork.user.User;
 public class SocialNetworkDemo {
 
     public static void main(String[] args) {
+        /*
         User user1 = new User("user1");
         System.out.println(user1.getName()); // user1
         System.out.println(user1.getAge()); // null
@@ -45,6 +46,7 @@ public class SocialNetworkDemo {
         System.out.println(socialNetwork.getGroups()); // [group1, newGroup]
 
         System.out.println(socialNetwork.getFeedForUser(user2)); // [title1]
+        */
     }
 
 }
