f222d196d49d7f67a8a4ff20f660492a621c5041
Raul.Vesinurm <Raul.Vesinurm@ttu.ee>
2019-02-27 12:38:37 +0200

    TK2

--- TK2/src/ee/taltech/iti0202/tk2/Exam.java
+++ TK2/src/ee/taltech/iti0202/tk2/Exam.java
@@ -76,7 +76,7 @@ public class Exam {
         String end = str.substring(str.length() - n);
         String result = "";
         for (int i = 0; i < n; i++) {
-            result += end;
+            result = result + end;
         }
         return result;
 
---
+++ TK2/src/ee/taltech/iti0202/tk2/art/Painting.java
@@ -0,0 +1,4 @@
+package ee.taltech.iti0202.tk2.art;
+
+public class Painting {
+}
