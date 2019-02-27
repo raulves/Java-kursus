ed02538a8506c66f2e6adb8be5632fcf405ee6d3
Raul.Vesinurm <Raul.Vesinurm@ttu.ee>
2019-02-27 13:26:30 +0200

    TK2

--- TK2/src/ee/taltech/iti0202/tk2/art/Collector.java
+++ TK2/src/ee/taltech/iti0202/tk2/art/Collector.java
@@ -1,5 +1,21 @@
 package ee.taltech.iti0202.tk2.art;
 
-public class Collector {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Collector {
+    private List<Painting> paintings = new ArrayList<>();
+
+    public boolean addPainting(Painting painting) {
+        List<String> paint = new ArrayList<>();
+        return false;
+    }
+
+    public boolean sellPainting(Painting painting, Collector fellowCollector) {
+        return true;
+    }
+
+    public List<Painting> getPaintings() {
+        return paintings;
+    }
 }
