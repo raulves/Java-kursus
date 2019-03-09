affbf0f449dee6dbe3cf522bfe8da950028b5b7c
Raul Vesinurm <raul.vesinurm@ttu.ee>
2019-03-09 15:07:28 +0200

    EX06

--- EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
+++ EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
@@ -1,6 +1,10 @@
 package ee.taltech.iti0202.files.morse;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MorseTranslator {
 
