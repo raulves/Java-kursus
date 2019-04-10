1b414bec6b7900d052dde2979198a49f88dc3f8d
Raul Vesinurm <raul.vesinurm@ttu.ee>
2019-04-10 16:25:08 +0300

    PR11

--- PR11Testing/src/ee/taltech/iti0202/testing/SentenceTest.java
+++ PR11Testing/src/ee/taltech/iti0202/testing/SentenceTest.java
@@ -1,7 +1,11 @@
 package ee.taltech.iti0202.testing;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 
 public class SentenceTest {
 
