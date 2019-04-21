a4030a32c1d8ebd7dd2c0deb0d0f6f4bd0f6bea2
Raul Vesinurm <raul.vesinurm@ttu.ee>
2019-04-21 16:18:34 +0300

    pr12

--- PR12Recursion/src/ee/taltech/iti0202/recursion/Recursion.java
+++ PR12Recursion/src/ee/taltech/iti0202/recursion/Recursion.java
@@ -16,10 +16,6 @@
                 return "";
             }
 
-            if (!word.contains("(") && !word.contains(")")) {
-                return "";
-            }
-
             if (word.substring(0, 1).equals("(") && word.contains(")")) {
                 return word.substring(0, word.lastIndexOf(")") + 1);
             }
@@ -65,17 +61,22 @@
 
             System.out.println(parentheses("I am useless text(Find me), yet again useless")); // "(Find me)"
             System.out.println(parentheses("This doesn't have any parentheses.")); // ""
-            System.out.println(parentheses("What do you do if (sentence has (many parentheses) and where it ends)")); // "(sentencce has (many parentheses) and where it ends)"
+            System.out.println(parentheses("What do you do if (sentence has (many parentheses) and " +
+                    "where it ends)")); // "(sentencce has (many parentheses) and where it ends)"
             // System.out.println();
 
             System.out.println(removeDuplicates("aabbccddeeffgg")); // "abcdefg"
             System.out.println(removeDuplicates("foakfjdirmdogmvooasf")); // "foakfjdirmdogmvoasf"
-            System.out.println(removeDuplicates("ilIliiiiilIili1lilllliiilil1ilili111111lili1")); // "ilIlilIili1lililil1ilili1lili1"
+            System.out.println(removeDuplicates("ilIliiiiilIili1lilllliiilil1ilili111111lili1"));
+            // "ilIlilIili1lililil1ilili1lili1"
             // System.out.println();
 
-            System.out.println(pidginfy("Kūle'a ka'ōpopo'ōpiopio ma luna o ka'īlio palaualelo.")); // "Kūle'a ka'ōpopo'ōpiopio ma luna o ka'lio palaualelo."
-            System.out.println(pidginfy("kasmdfastu naidsfnasidn weraiskdfm sdfasdf''assdffaksndfasdf")); // "kamau nainain weraikm a''aakna"
-            System.out.println(pidginfy("He nani ka'iliahi, akā,'a'ohe mea'ala, no ka mea he mea'alala'i ka raiki, pono nō ka'ohe."));
+            System.out.println(pidginfy("Kūle'a ka'ōpopo'ōpiopio ma luna o ka'īlio palaualelo."));
+            // "Kūle'a ka'ōpopo'ōpiopio ma luna o ka'lio palaualelo."
+            System.out.println(pidginfy("kasmdfastu naidsfnasidn weraiskdfm sdfasdf''assdffaksndfasdf"));
+            // "kamau nainain weraikm a''aakna"
+            System.out.println(pidginfy("He nani ka'iliahi, akā,'a'ohe mea'ala, no ka mea he " +
+                    "mea'alala'i ka raiki, pono nō ka'ohe."));
             //"He nani ka'iliahi, akā,'a'ohe mea'ala, no ka mea he mea'alala'i ka raiki, pono nō ka'ohe."
 
         }
