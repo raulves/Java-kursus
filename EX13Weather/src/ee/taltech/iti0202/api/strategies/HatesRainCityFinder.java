77b422cad07710dcab4b2fbd38859113f3d85d21
Raul Vesinurm <raul.vesinurm@ttu.ee>
2019-05-02 09:01:53 +0300

    ex13

--- EX13Weather/src/ee/taltech/iti0202/api/strategies/HatesRainCityFinder.java
+++ EX13Weather/src/ee/taltech/iti0202/api/strategies/HatesRainCityFinder.java
@@ -2,12 +2,35 @@ package ee.taltech.iti0202.api.strategies;
 
 import ee.taltech.iti0202.api.destinations.City;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class HatesRainCityFinder implements CityFinderStrategy {
     @Override
     public Optional<City> findBestCity(List<City> candidateCities) {
-        return Optional.empty();
+        List<City> acceptableCities = candidateCities.stream().filter(city -> city.getAverageHumidity() <= 80).collect(Collectors.toList());
+
+        List<City> bestCities = new ArrayList<>();
+
+
+        for (City acceptableCity : acceptableCities) {
+            int count = 0;
+            for (Integer code : acceptableCity.getWeatherCodes()) {
+                if (code / 100 == 5) {
+                    count++;
+                }
+            }
+            if (count <= 1) {
+                bestCities.add(acceptableCity);
+            }
+        }
+
+        return Optional.ofNullable(bestCities.get(0));
     }
+
+
+
+
 }
