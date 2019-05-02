c8c69b9ff580bb2250a3dcb06085d577138c147e
Raul Vesinurm <raul.vesinurm@ttu.ee>
2019-05-02 18:14:50 +0300

    pr14

--- PR14CakeOrder/src/ee/taltech/iti0202/cakeorder/CakeOrderProcessor.java
+++ PR14CakeOrder/src/ee/taltech/iti0202/cakeorder/CakeOrderProcessor.java
@@ -1,6 +1,15 @@
 package ee.taltech.iti0202.cakeorder;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
+
+import java.util.List;
+
 public class CakeOrderProcessor {
+    private final List<String> milkProducts = List.of("milk", "cream-cheese", "yoghurt");
     public enum CakeOrderProcessorType {
         MAKE_DAIRY_FREE,
         COUNT_TOTAL_SUM,
@@ -15,6 +24,86 @@ public class CakeOrderProcessor {
     }
 
     public String process(String jsonInput) {
-        return null;
+        JsonObject jsonObject = new JsonParser().parse(jsonInput).getAsJsonObject();
+        JsonArray array = jsonObject.getAsJsonArray("cakes");
+
+        if (type.equals(CakeOrderProcessorType.MAKE_DAIRY_FREE)) {
+            for (JsonElement cake : array) {
+                JsonArray ingredients = cake.getAsJsonObject().get("ingredients").getAsJsonArray();
+                double milkIngredientsTotal = 1.0;
+                for (int i = 0; i < ingredients.size(); i++) {
+                    if (milkProducts.contains(ingredients.get(i).getAsString())) {
+                        ingredients.set(i, new JsonPrimitive("plant-" + ingredients.get(i).getAsString()));
+                        milkIngredientsTotal += 0.1;
+                    }
+                }
+                cake.getAsJsonObject().add("price",
+                        new JsonPrimitive(cake.getAsJsonObject().get("price").getAsDouble() * milkIngredientsTotal));
+            }
+        }
+        return jsonObject.toString();
     }
+
+
+    public static void main(String[] args) {
+        CakeOrderProcessor cp = new CakeOrderProcessor(CakeOrderProcessorType.MAKE_DAIRY_FREE);
+        String input = "{\n" +
+                "  \"cakes\": [\n" +
+                "    {\n" +
+                "      \"name\": \"Sacher\",\n" +
+                "      \"BBD\": \"2019-04-29\",\n" +
+                "      \"price\": 14.00,\n" +
+                "      \"kg\": 2.00,\n" +
+                "      \"ingredients\": [\"flour\", \"chocolate\", \"milk\", \"sugar\", \"eggs\"]\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"New York Cheesecake\",\n" +
+                "      \"BBD\": \"2019-04-30\",\n" +
+                "      \"price\": 10.00,\n" +
+                "      \"kg\": 1.50,\n" +
+                "      \"ingredients\": [\"flour\", \"cream-cheese\", \"milk\", \"sugar\", \"eggs\"]\n" +
+                "\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+        System.out.println(cp.process(input));
+
+        /*
+        JsonObject jsonObject = new JsonParser().parse(input).getAsJsonObject();
+        JsonArray array = jsonObject.getAsJsonArray("cakes");
+        System.out.println(array.get(0).getAsJsonObject().get("ingredients").getAsJsonArray());
+
+        Gson gson = new Gson();
+        JsonObject jsonObject2 = gson.fromJson(input, JsonObject.class);
+        double newPrice = 17.89;
+        JsonElement je = new JsonPrimitive(newPrice);
+        System.out.println(jsonObject2.getAsJsonArray("cakes").get(0));
+
+        jsonObject2.getAsJsonArray("cakes").get(0).getAsJsonObject().add("price", je);
+        jsonObject.getAsJsonArray("cakes").get(0).getAsJsonObject().add("price", je);
+        System.out.println(jsonObject2.getAsJsonArray("cakes").get(0));
+        System.out.println("-------------------");
+
+        List<String> milkProducts = List.of("milk", "cream-cheese", "yoghurt");
+        for (JsonElement cake : array) {
+            JsonArray ingredients = cake.getAsJsonObject().get("ingredients").getAsJsonArray();
+
+            for (int i = 0; i < ingredients.size(); i++) {
+                if (milkProducts.contains(ingredients.get(i).getAsString())) {
+                    System.out.println(ingredients.get(i).getAsString());
+                    ingredients.set(i, new JsonPrimitive("plant-" + ingredients.get(i).getAsString()));
+
+                    System.out.println("MILK");
+                } else {
+                    System.out.println("NO Milk");
+                }
+
+            }
+            System.out.println(ingredients);
+        }
+        System.out.println(array.toString());
+        System.out.println(14.0 * 1.1);
+        */
+    }
+
 }
