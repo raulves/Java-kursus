7b54f01e90ccb6dbc1f4ffc6407357fbb80513da
Raul Vesinurm <raul.vesinurm@ttu.ee>
2019-05-16 15:18:50 +0300

    pr15

--- PR15Generics/src/ee/taltech/iti0202/generics/Animal.java
+++ PR15Generics/src/ee/taltech/iti0202/generics/Animal.java
@@ -3,7 +3,6 @@ package ee.taltech.iti0202.generics;
 public abstract class Animal {
 
   protected String name;
-  
   public Animal(String name) {
     this.name = name;
   }
--- PR15Generics/src/ee/taltech/iti0202/generics/AnimalBox.java
+++ PR15Generics/src/ee/taltech/iti0202/generics/AnimalBox.java
@@ -1,18 +1,13 @@
 package ee.taltech.iti0202.generics;
 
 import ee.taltech.iti0202.generics.foods.Food;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 public class AnimalBox<T extends Animal> {
 
-    // private List<T> animalInBox = new ArrayList<>();
     private T animalBox;
 
     public void put(T animal) {
-        // animalInBox.add(animal);
         if (animalBox == null) {
             animalBox = animal;
         }
